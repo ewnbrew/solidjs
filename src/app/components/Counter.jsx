773bcc12f425c7ecca0a178a7a9b70ec779d0a08
Erwin <geronimo44rr@gmail.com>
2023-12-16 13:51:18 +0700

    create counter using createSignal()

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,9 +1,33 @@
-import { createSignal } from "solid-js";
+import { createSignal, createEffect } from "solid-js";
 
 var Counter = () => {
   const [count, setCount] = createSignal(0);
-  console.log(count());
-  return
+  
+  createEffect(() => {
+    console.log(count());
+  });
+
+  var tambahCounter = () => {
+    setCount(count() + 1);
+  }
+
+  var kurangCounter = () => {
+    setCount(count() - 1);
+    if (count() === -1) {
+      alert('cukup bang');
+      return;
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <h3> { count() } </h3>
+        <button onclick={ tambahCounter }>Tambah</button>
+        <button onclick={ kurangCounter }>Tambah</button>
+      </div>
+    </>
+  )
 }
 
 export default Counter;
